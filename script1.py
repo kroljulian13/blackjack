2a7b746125bfa28b18963382236b07bbf4cd5c55
Sylwester Guzek <Sylwester.guzek@wp.pl>
2017-12-21 16:21:23 +0100

    initial commit part II

---
+++ script1.py
@@ -0,0 +1,173 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+#funkcja zliczania punktów (ogarnia kombinacje tylko do 2 asów)
+# A = 11 lub 1  
+# figura z A 21
+# split not implemented
+
+# to implement double bet when getting 11 (hit with only 1 card allowed)
+
+cardsDef11 = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":10,"D":10,"K":10,"A":11}
+cardsDef1  = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":10,"D":10,"K":10,"A":1}
+
+def score(cards):
+    counter = 0
+    counter1 = 0
+    counter2 = 0
+    counter3 = 0
+
+    if "A" in cards: 
+        aCount=0
+        for card in cards:
+            counter1 += cardsDef1[card]
+            counter2 += cardsDef11[card]
+            counter3 += cardsDef1[card]
+            if card == "A": 
+                aCount+=1
+                if aCount == 2:
+                    counter3 = counter3+10             
+
+        result =  [counter1, counter2, counter3]
+
+        result=list(set(result))  # removing double values
+        return sorted(result)
+
+    else:
+        for card in cards:
+            counter += cardsDef11[card]
+        return [counter]
+
+def bestScore(scores):
+    #asuming that input values are sorted
+    result = scores[0]
+    for item in reversed(scores):
+        if item <= 21:
+            return item
+
+    return result
+
+
+def hiLo(init, krupier, user):
+    rate = init
+    for item in krupier:
+        if cardsDef11[item] > 9:
+            rate -=1
+        if cardsDef11[item] < 7:
+            rate +=1
+    for item in user:
+        if cardsDef11[item] > 9:
+            rate -=1
+        if cardsDef11[item] < 7:
+            rate +=1
+    
+    return rate
+
+def game(dobraneKarty, money, baseBet): # az do rozdania wszystkich kart w tali
+    talia=['2','2','2','2',
+           '3','3','3','3',
+           '4','4','4','4',
+           '5','5','5','5',
+           '6','6','6','6',
+           '7','7','7','7',
+           '8','8','8','8',
+           '9','9','9','9',
+           '10','10','10','10',
+           'J','J','J','J',
+           'D','D','D','D',
+           'K','K','K','K',
+           'A','A','A','A'
+            ]
+    #extending to 4 decks
+    talia.extend(talia)
+    talia.extend(talia)
+    
+    rozdanie = 0
+    zostaloKart = 0
+    totalWin=0
+    HL = 0 
+    realHL =0
+    
+    while len(talia) > 10:
+        rozdanie +=1
+        zostaloKart = len(talia)
+
+        #pojedyncze rozdanie
+        krupier = []
+        user = []
+        result = 0
+        baseBetHL=baseBet+realHL*baseBet
+
+        krupier=random.sample(talia, 1)
+        for item in krupier:
+            talia.remove(item)
+
+        #dobieranie kart przez usera
+        user=random.sample(talia, 2)
+        for item in user:
+            talia.remove(item)
+
+        hitLimit = 0
+        # if bestScore(score(krupier)) < 7:
+        #     hitLimit=5
+        while bestScore(score(user)) < (17-dobraneKarty-hitLimit):
+            newCard=random.sample(talia, 1)
+            for item in newCard:
+                talia.remove(item)
+            user.extend(newCard)
+
+        #dobieranie kart przez krupiera
+        if bestScore(score(user)) <= 21:
+            while bestScore(score(krupier)) < bestScore(score(user)):
+                newCard=random.sample(talia, 1)
+                for item in newCard:
+                    talia.remove(item)
+                krupier.extend(newCard)
+            # czy remis
+            if bestScore(score(krupier)) == bestScore(score(user)):
+                result = 0
+
+            elif bestScore(score(krupier)) > 21:
+                result = 1
+                totalWin+=1
+                money += baseBetHL
+            
+            else:
+                money -= baseBetHL    
+
+        else:
+            money -= baseBetHL
+
+        HL = hiLo(HL, krupier, user)
+        realHL = HL/4
+
+        print("\nUser: "+str(bestScore(score(user))),user)
+        print("Krupier: "+str(bestScore(score(krupier))),krupier)
+        # print("Talia: ",talia)
+        print("Result: "+str(totalWin)+"/"+str(rozdanie))
+        print("Zostało kart:", len(talia))
+        print("real HI-LO: "+str(realHL))
+        print("money: ",money)
+        print("bet :",baseBetHL)
+
+    return money
+
+# cykl 1..2..3 (kilkaset rozdań) - dobieram 0..1..2 kart gdy score < 17
+    #----- losuj karte dla krupiera -> Input 1
+    #----- losuj karty dla usera -> Input 2
+    # dobieraj karty dla krupiera az > Input 2
+    # zapisz wygrana/przegrana -> Output
+
+money = []
+# balance = 1000
+for i in range(1):
+    baseBet = 10
+    # balance =  game(0, balance, baseBet)
+    money.append( game(0, 1000, baseBet))
+
+print(money)
+
+plt.hist(money, normed=True, bins=30)
+plt.ylabel('Probability')
+plt.show()
---
+++ test.py
@@ -0,0 +1,60 @@
+import unittest
+from script1 import score, bestScore, hiLo
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_result1(self):
+        self.assertEqual( score(["2","3"]) , [5])
+
+    def test_result1a(self):
+        self.assertEqual( score(["D","3"]) , [13])
+
+    def test_result1b(self):
+        self.assertEqual( score(["3","3","5","5","10"]) , [26])
+
+    def test_result2(self):
+        self.assertEqual( score(["A","A"]) , [2, 12, 22])
+
+    def test_result3(self):
+        self.assertEqual( score(["K","A"]) , [11, 21])
+
+    def test_result4(self):
+        self.assertEqual( score(["A","9"]) , [10, 20])
+
+    def test_result4a(self):
+        self.assertEqual( score(["A","2"]) , [3, 13])
+
+    def test_result4b(self):
+        self.assertEqual( score(["A","2","3"]) , [6, 16])       
+
+    def test_result4c(self):
+        self.assertEqual( score(["A","2","3","4"]) , [10, 20])     
+
+    def test_result5(self):
+        self.assertEqual( score(["A","9","A"]) , [11, 21, 31])
+
+# ------------------------------------------------------------------
+    def test_result6a(self):
+        self.assertEqual( bestScore([11]) , 11)
+
+    def test_result6b(self):
+        self.assertEqual( bestScore([26]) , 26)
+
+    def test_result6(self):
+        self.assertEqual( bestScore([11, 21, 31]) , 21)
+
+# ------------------------------------------------------------------
+    def test_result7(self):
+        self.assertEqual( hiLo(["A","2","3"],["3","7","10"]) , 1)
+
+
+
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
